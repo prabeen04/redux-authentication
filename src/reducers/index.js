57733f7eba72b8a6e2fbf84c1c6ca417c188335b
Prabeen04 <prabeen.strange@gmail.com>
2018-04-17 09:07:13 +0530

    sessionReducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers, applyMiddleware } from  'redux'
+import { sessionReducer } from './session_reducer' 
 
 export const rootReducer = combineReducers({
-    
+    sessionReducer
 })
---
+++ src/reducers/session_reducer.js
@@ -0,0 +1,11 @@
+const initialState={
+    session: false
+}
+
+export const sessionReducer = (state=initialState, action) => {
+    switch(action.type){
+        default: 
+            return state
+    }
+    return state
+}
