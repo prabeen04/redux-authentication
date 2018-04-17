73e0e60e8beb38ea009e7495a886c20bf278c7e0
Prabeen04 <prabeen.strange@gmail.com>
2018-04-17 21:54:19 +0530

    redux implemented in login component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { loginUser } from '../../actions/user_login'
 class Login extends Component {
   render() {
     return (
       <div>
          <h1>login commponent</h1>
-         <button onClick={()=>this.props.loginUser}>Login</button>
+         <button onClick={()=>this.props.loginUser()}>Login</button>
       </div>
     )
   }
 }
 
-export default Login;
+const mapStateToProps = (state, ownProps) => {
+    return {
+      isLoggedIn: state.sessionReducer.isLoggedIn
+    }
+  }
+  const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+      loginUser
+    }, dispatch)
+  }
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
