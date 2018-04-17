5087cb6678cd51f23af76b8aad734ac132d18861
Prabeen04 <prabeen.strange@gmail.com>
2018-04-17 09:38:19 +0530

    react-router configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { bindActionCreators } from 'redux'
 import { loginUser } from './actions/user_login'
 import './App.css';
 
 class App extends Component {
-  componentDidMount(){
-    this.props.loginUser()
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.loginUser()
+    }, 4000)
+
   }
   render() {
     return (
       <div>
-       { this.props.isLoggedIn
-       ?<h1>App Component</h1>
-      : <h2>you are not logged in...</h2>}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home}/>
+        </Switch>
+        {this.props.isLoggedIn
+          ? <h1>App Component</h1>
+          : <h2>you are not logged in...</h2>}
       </div>
     );
   }
 }
 
-const mapStateToProps =(state, ownProps) => {
+const mapStateToProps = (state, ownProps) => {
   return {
     isLoggedIn: state.sessionReducer.isLoggedIn
   }
@@ -30,3 +38,19 @@ const mapDispatchToProps = (dispatch) => {
   }, dispatch)
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+ const Home = () => {
+  return (
+    <div>
+      <h1>home commponent</h1>
+    </div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>
+      <h1>login commponent</h1>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -8,8 +9,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
-       <App/> 
+        <Router>
+            <App />
+        </Router>
     </Provider>
-,
- document.getElementById('root'));
+    ,
+    document.getElementById('root'));
 registerServiceWorker();
