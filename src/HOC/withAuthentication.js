1819da4fa69db0a85091a9c4cb1bd5298600a9e3
Prabeen04 <prabeen.strange@gmail.com>
2018-04-18 22:43:41 +0530

    authentication HOC cleaned

--- src/HOC/withAuthentication.js
+++ src/HOC/withAuthentication.js
@@ -1,26 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-// Authentication HOC
-const Authentication = (WrappedComponent) =>{
-  return class WithAuthentication extends React.Component {
-    constructor(props) {
-      super(props)
-
-      // In this case the user is hardcoded, but it could be loaded from anywhere.
-      // Redux, MobX, RxJS, Backbone...
+const Authentication = (WrappedComponent) => {
+    return class WithAuthentication extends React.Component {
+        constructor(props) {
+            super(props)
+        }
+        render() {
+            return <WrappedComponent isLoggedIn={this.props.isLoggedIn} {...this.props} />
+        }
     }
-    render() {
-      if (this.state.user) {
-        return <WrappedComponent isLoggedIn={this.props.isLoggedIn} {...this.props} />
-      } else {
-        return <h1>You are Not Authenticated...</h1>
-      }
-    }
-  }
 }
 const mapStateToProps = (state) => {
-    return{
+    return {
         isLoggedIn: state.sessionReducer.isLoggedIn
     }
 }
