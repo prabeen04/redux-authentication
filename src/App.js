18ae4b9bab87bba2715044f7ba18caab62823ee9
Prabeen04 <prabeen.strange@gmail.com>
2018-04-19 19:59:17 +0530

    protected component creater

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { history } from 'history';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { loginUser } from './actions/user_login';
-import Dashboard from './components/dashboard/dashboard'
-import Login from './components/login/login'
+import PrivateRoute from './components/auth/authentication';
+import Dashboard from './components/dashboard/dashboard';
+import Protected from './components/protected/protected';
+import Login from './components/login/login';
+
 import './App.css';
 
 class App extends Component {
@@ -16,6 +19,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Dashboard}/>
           <Route exact path="/login" component={Login}/>
+          <PrivateRoute path="/protected" component={Protected}/>
         </Switch>
       </div>
     );
---
+++ src/components/protected/protected.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Protected extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Protected Route</h1>
+      </div>
+    )
+  }
+}
+
+export default Protected;
