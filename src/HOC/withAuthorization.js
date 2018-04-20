1ca5724e142775376716b4d6b56c616cff08c1d0
Prabeen04 <prabeen.strange@gmail.com>
2018-04-20 23:19:20 +0530

    authorization HOC creted

---
+++ src/HOC/withAuthorization.js
@@ -0,0 +1,25 @@
+
+// Authorization HOC
+const Authorization = (WrappedComponent, allowedRoles) =>
+  return class WithAuthorization extends React.Component {
+    constructor(props) {
+      super(props)
+
+      // In this case the user is hardcoded, but it could be loaded from anywhere.
+      // Redux, MobX, RxJS, Backbone...
+      this.state = {
+        user: {
+          name: 'vcarl',
+          role: 'admin'
+        }
+      }
+    }
+    render() {
+      const { role } = this.state.user
+      if (allowedRoles.includes(role)) {
+        return <WrappedComponent {...this.props} />
+      } else {
+        return <h1>No page for you!</h1>
+      }
+    }
+  }
