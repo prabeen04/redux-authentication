29fb602758b5368ce546bb8977718ad24e137139
Prabeen04 <prabeen.strange@gmail.com>
2018-04-21 18:59:36 +0530

    login action and reducer created

--- src/actions/user_login.js
+++ src/actions/user_login.js
@@ -1,8 +1,28 @@
+import { LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE } from '../action_types/login_actiontypes';
+import axios from 'axios';
+
+const baseURL = 'https://prabeen-restapi.herokuapp.com/api/login';
+
 export const loginUser = (credential) => {
     console.log(credential)
     return (dispatch) => {
-       return dispatch({
-            type: "LOGIN_USER"
+        dispatch({
+            type: LOGIN
         })
+
+        return axios.post(`${baseURL}`, credential)
+            .then(response => {
+                dispatch({
+                    type: LOGIN_SUCCESS,
+                    payload: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                dispatch({
+                    type: LOGIN_SUCCESS,
+                    payload: error
+                })
+            })
     }
 }
--- src/reducers/session_reducer.js
+++ src/reducers/session_reducer.js
@@ -1,12 +1,21 @@
-const initialState={
-    isLoggedIn: false
+import { LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE } from '../action_types/login_actiontypes';
+
+const initialState = {
+    isLoggedIn: false,
+    token: '',
+    loggingIn: false,
+    error: ''
 }
 
-export const sessionReducer = (state=initialState, action) => {
-    switch(action.type){
-        case "LOGIN_USER": 
-            return Object.assign({}, state, { isLoggedIn: true})
-        default: 
+export const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN:
+            return Object.assign({}, state, { loggingIn: true })
+        case LOGIN_SUCCESS:
+            return Object.assign({}, state, { loggingIn: false, token: action.payload, isLoggedIn: true })
+        case LOGIN_FAILURE:
+            return Object.assign({}, state, { loggingIn: false })
+        default:
             return state
     }
     return state
