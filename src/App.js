d4b7aa3f8d3fb133b181254097aaf13fe162ce23
Prabeen04 <prabeen.strange@gmail.com>
2018-04-21 21:57:47 +0530

    steper form added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/auth/authentication';
 import Dashboard from './components/dashboard/dashboard';
+import CompleteRegistration from './components/complete_registration/complete_registration';
 import Login from './components/login/login';
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends Component {
       <div>   
         <Switch>
           <Route exact path="/login" component={Login} />
+          <Route exact path="/completeRegistration" component={CompleteRegistration} />
           <PrivateRoute  path="/" component={Dashboard} />
        </Switch>   
       </div> 
--- src/components/complete_registration/complete_registration.js
+++ src/components/complete_registration/complete_registration.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react'
+import './complete_registration.css';
+import { Steps, Button, message } from 'antd';
+const Step = Steps.Step;
+
+const steps = [{
+  title: 'First',
+  content: 'First-content',
+}, {
+  title: 'Second',
+  content: 'Second-content',
+}, {
+  title: 'Last',
+  content: 'Last-content',
+}];
 
 class CompleteRegistration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: 0,
+    };
+  }
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+  prev() {
+    const current = this.state.current - 1;
+    this.setState({ current });
+  }
+
   render() {
+    const { current } = this.state;
     return (
-      <div>
-        <h1>Complete Registration Component</h1>
+      <div className="complete-registration">
+        <div>
+          <Steps current={current}>
+            {steps.map(item => <Step key={item.title} title={item.title} />)}
+          </Steps>
+          <div className="steps-content">{steps[this.state.current].content}</div>
+          <div className="steps-action">
+            {
+              this.state.current < steps.length - 1
+              &&
+              <Button type="primary" onClick={() => this.next()}>Next</Button>
+            }
+            {
+              this.state.current === steps.length - 1
+              &&
+              <Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
+            }
+            {
+              this.state.current > 0
+              &&
+              <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
+                Previous
+            </Button>
+            }
+          </div>
+        </div>
       </div>
     )
   }
