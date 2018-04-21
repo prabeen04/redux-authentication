9c328f2eb6410d1e2fb177e515ec71d5f1e5172b
Prabeen04 <prabeen.strange@gmail.com>
2018-04-22 00:45:18 +0530

    unathorized message moved inside component

--- src/HOC/withAuthorization.js
+++ src/HOC/withAuthorization.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
 import { message } from 'antd';
+
 // Authorization HOC
 const Authorization = (WrappedComponent, allowedRoles) =>{
     class WithAuthorization extends React.Component {
         constructor(props) {
           super(props)
-    
-          // In this case the user is hardcoded, but it could be loaded from anywhere.
-          // Redux, MobX, RxJS, Backbone...
           this.state = {
             user: {
               role: 'admin1'
             }
           }
+          this.unauthorizedMessage = this.unauthorizedMessage.bind(this);
         }
+       unauthorizedMessage = () => message.error('You are Unauthorized to view this page !!!');
         render() {
           const { role } = this.state.user
           if (allowedRoles.includes(role)) {
             return <WrappedComponent {...this.props} />
           } else {
             console.log('not authorized');
-           () => message.error('Processing complete!');
+            this.unauthorizedMessage();
             return <Redirect to='/'/>
           }
         }
