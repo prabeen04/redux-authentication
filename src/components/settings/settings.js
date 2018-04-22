6f7d60059dd2e0e3cf0075c503cc67e3ed6adc06
Prabeen04 <prabeen.strange@gmail.com>
2018-04-22 11:19:50 +0530

    tab content styles creaetd

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -7,15 +7,19 @@ const TabPane = Tabs.TabPane;
 class Settings extends Component {
   render() {
     return (
-      <div>
+      <div className="settings-wrapper">
         <Tabs
           defaultActiveKey="2"
-          tabBarStyle={{display: 'flex', justifyContent: 'center'}}>
-          <TabPane tab={<span><Icon type="apple" />Tab 1</span>} key="1" style={{color: 'red'}}>
-            Tab 1
+          tabBarStyle={{ display: 'flex', justifyContent: 'center' }}>
+          <TabPane tab={<span><Icon type="apple" />Apple Setting</span>} key="1">
+            <div className="tab-content">
+
+            </div>
           </TabPane>
-          <TabPane tab={<span><Icon type="android" />Tab 2</span>} key="2">
-            Tab 2
+          <TabPane tab={<span><Icon type="android" />Android Setting</span>} key="2">
+            <div className="tab-content">
+
+            </div>
           </TabPane>
         </Tabs>
       </div>
