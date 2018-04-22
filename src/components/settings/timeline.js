4a049437331cba070f591731eeb66fc7ceea9312
Prabeen04 <prabeen.strange@gmail.com>
2018-04-22 11:32:49 +0530

    ant timeline added

--- src/components/settings/timeline.js
+++ src/components/settings/timeline.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { Timeline, Icon } from 'antd';
 
 class SettingTimeline extends Component {
-  render() {
-    return (
-      <div>
-        <h1>SettingTimeline component</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>Project Status</h1>
+                <Timeline>
+                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+                    <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+                    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">Technical testing 2015-09-01</Timeline.Item>
+                    <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+                </Timeline>
+            </div>
+        )
+    }
 }
 
 export default SettingTimeline;
