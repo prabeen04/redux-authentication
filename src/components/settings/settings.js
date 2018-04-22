2b00cf6673641f00cfebd9691ba3bf8f844a940d
Prabeen04 <prabeen.strange@gmail.com>
2018-04-22 10:17:35 +0530

    ant tab added in settings component

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { Tabs, Icon } from 'antd';
+import './settings.css';
+
+const TabPane = Tabs.TabPane;
 
 class Settings extends Component {
   render() {
     return (
       <div>
-        <h1>Settings Component</h1>
+        <Tabs defaultActiveKey="2">
+          <TabPane tab={<span><Icon type="apple" />Tab 1</span>} key="1">
+            Tab 1
+          </TabPane>
+          <TabPane tab={<span><Icon type="android" />Tab 2</span>} key="2">
+            Tab 2
+          </TabPane>
+        </Tabs>
       </div>
     )
   }
