ea135276c6bf7273350c8b6e526b69e959445135
Prabeen04 <prabeen.strange@gmail.com>
2018-04-23 21:36:48 +0530

    register action created

--- src/actions/register_action.js
+++ src/actions/register_action.js
@@ -1,1 +1,25 @@
-import { LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE } from '../action_types/register_actiontypes';
+import { REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE } from '../action_types/register_actiontypes';
+import axios from 'axios';
+
+const baseURL = 'https://prabeen-restapi.herokuapp.com/api/register';
+
+export const registerUser = (credential) => {
+    return (dispatch) => {
+        dispatch({
+            type: REGISTER
+        })
+        return axios.post(`${baseURL}`, credential)
+        .then(response => {
+                dispatch({
+                    type: REGISTER_SUCCESS,
+                    payload: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                dispatch({
+                    type: REGISTER_FAILURE
+                })
+            })
+    }
+}
