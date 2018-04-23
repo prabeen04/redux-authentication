b608cf1b020ea2bada15126a283ffae07a33bee4
Prabeen04 <prabeen.strange@gmail.com>
2018-04-23 20:12:11 +0530

    logout action created

---
+++ src/actions/logout_action.js
@@ -0,0 +1,25 @@
+import { LOGOUT_USER, LOGOUT_USER_SUCCESS, LOGOUT_USER_FAILURE } from '../action_types/logout_actiontypes';
+
+const baseURL = 'https://prabeen-restapi.herokuapp.com/api/login';
+
+export const logoutUser = (user) => {
+    return (dispatch) => {
+        dispatch({
+            type: LOGOUT_USER
+        })
+        return axios.post(`${baseURL}`, credential)
+        .then(response => {
+            localStorage.setItem('token', response.data)
+                dispatch({
+                    type: LOGOUT_USER_SUCCESS,
+                    payload: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                dispatch({
+                    type: LOGOUT_USER_FAILURE
+                })
+            })
+    }
+}
