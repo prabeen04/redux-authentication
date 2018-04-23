f5c23566eafb21fd4203652f51b2c86cba8ebe0b
Prabeen04 <prabeen.strange@gmail.com>
2018-04-23 20:22:42 +0530

    private component connnected to redux

--- src/components/private/private.js
+++ src/components/private/private.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { withRouter} from 'react-router-dom';
 import { BackTop, Button } from 'antd';
+import { logoutUser } from '../../actions/logout_action';
 import './private.css';
 
 class Private extends Component {
@@ -21,4 +24,9 @@ class Private extends Component {
   }
 }
 
-export default withRouter(Private);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    logoutUser
+  }, dispatch)
+}
+export default withRouter(connect(null, mapDispatchToProps)(Private));
