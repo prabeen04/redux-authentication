bdc63ca8c3bb1e87f58c7c6b889c272cf8112661
Prabeen04 <prabeen.strange@gmail.com>
2018-04-23 21:41:23 +0530

    register reducer create

--- src/reducers/register_reducer.js
+++ src/reducers/register_reducer.js
@@ -1,1 +1,20 @@
 import { REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE } from '../action_types/register_actiontypes';
+const initialState = {
+    isRegistering: false,
+    user: {},
+    isError: ''
+}
+
+export const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case REGISTER:
+            return Object.assign({}, state, { isRegistering: true })
+        case REGISTER_SUCCESS:
+            return Object.assign({}, state, { isRegistering: false, user: action.payload })
+        case REGISTER_FAILURE:
+            return Object.assign({}, state, { isRegistering: false, error:'Some error occoured...' })
+        default:
+            return state
+    }
+    return state
+}
